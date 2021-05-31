807753bc4c1746da1b64815ddeb83827907e8c01
Pedro Paes <iacapuca@gmail.com>
2021-05-31 12:01:58 -0300

    Fixed issue related to project path and db

--- app/src/main/java/alugacar/database/DatabaseSQLite.java
+++ app/src/main/java/alugacar/database/DatabaseSQLite.java
@@ -11,7 +11,7 @@ public class DatabaseSQLite{
     private DatabaseSQLite() throws SQLException {
         try {
             Class.forName("org.sqlite.JDBC");
-            this.connection = DriverManager.getConnection("jdbc:sqlite:app/db/alugacar.db");
+            this.connection = DriverManager.getConnection("jdbc:sqlite:alugacar.db");
         } catch (SQLException | ClassNotFoundException ex) {
             System.out.println("Database Connection Creation Failed : " + ex.getMessage());
         }
